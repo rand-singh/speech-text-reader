e59f57ae280d6a71d079e8b4489423efe6561c8b
Randeep Singh <randeeprattan90@gmail.com>
2020-05-27 18:14:34 +0100

    Markup and speech boxes

--- app.js
+++ app.js
@@ -1,1 +1,73 @@
-console.log("app js");
+const main = document.querySelector("main"),
+  voicesSelect = document.getElementById("voices"),
+  textarea = document.getElementById("text"),
+  readBtn = document.getElementById("read"),
+  toggleBtn = document.getElementById("toggle"),
+  closeBtn = document.getElementById("close"),
+  data = [
+    {
+      image: "./img/drink.jpg",
+      text: "I'm Thirsty",
+    },
+    {
+      image: "./img/food.jpg",
+      text: "I'm Hungry",
+    },
+    {
+      image: "./img/tired.jpg",
+      text: "I'm Tired",
+    },
+    {
+      image: "./img/hurt.jpg",
+      text: "I'm Hurt",
+    },
+    {
+      image: "./img/happy.jpg",
+      text: "I'm Happy",
+    },
+    {
+      image: "./img/angry.jpg",
+      text: "I'm Angry",
+    },
+    {
+      image: "./img/sad.jpg",
+      text: "I'm Sad",
+    },
+    {
+      image: "./img/scared.jpg",
+      text: "I'm Scared",
+    },
+    {
+      image: "./img/outside.jpg",
+      text: "I Want To Go Outside",
+    },
+    {
+      image: "./img/home.jpg",
+      text: "I Want To Go Home",
+    },
+    {
+      image: "./img/school.jpg",
+      text: "I Want To Go To School",
+    },
+    {
+      image: "./img/grandma.jpg",
+      text: "I Want To Go To Grandmas",
+    },
+  ];
+
+data.forEach(createBox);
+
+function createBox(item) {
+  const box = document.createElement("div"),
+    { image, text } = item;
+
+  box.classList.add("box");
+  box.innerHTML = `
+    <img src='${image}' alt="${text}" />
+    <p class="info">${text}</p>
+  `;
+
+  // @todo - speak event
+
+  main.appendChild(box);
+}
